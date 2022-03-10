d0de6deb78a23888567df718a55d042542a7647e
shanivdave <davesarah77@gmail.com>
2022-03-10 01:16:13 -0800

    This C program prints an exactly words, followed in a new line with printf

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- **/
+  * main - Prints string to standard error
+  * Return: 1
+  **/
+
 int main(void)
 {
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
