21e72c70ee5a7a6e3f36721f3a630cb0e01a9a4e
shanivdave <davesarah77@gmail.com>
2022-03-10 00:56:55 -0800

    This C program prints an exactly words, followed in a new line with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ *  * main - Prints string using printf
+ *   * Return: 0
+ *   **/
+
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
