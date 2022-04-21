ead38fb00eec945f39fc7e4d6cd46888436e5ee8
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-21 11:55:59 +0300

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,14 @@
-#include "lists.h"
+
+#include <stdio.h>
+
+void printfirst(void) __attribute__ ((constructor));
+/**
+ * printfirst - print before main
+ *
+ * Return: void
+ */
+void printfirst(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
