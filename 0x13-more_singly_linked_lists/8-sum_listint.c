cbb8cd0610b7599ef1193bd63842764c3577ddf6
shanivdave <32578797+shanivdave@users.noreply.github.com>
2022-04-25 12:16:29 +0300

    Update 8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,3 +1,22 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+
+/**
+ * sum_listint - returns the sum of all the data of a list
+ * @head: the head of the list
+ * Return: the sum, 0 if list is empty
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
